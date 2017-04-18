7d70d29dcf6d2a56477cba2e8203ee596177f0a9
Raeid Saqur <raeidsaqur@gmail.com>
2017-04-18 19:21:19 -0400

    Updated to Swift 3

--- MySampleTVProject/MyTVController.swift
+++ MySampleTVProject/MyTVController.swift
@@ -11,35 +11,35 @@ import UIKit
 
 class MyTVController: UITableViewController {
     
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("postCell", forIndexPath: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
         cell.textLabel?.text = "This is post \(indexPath.row)"
         //cell.textLabel?.text = words[indexPath.row]
         return cell
     }
     
     //MARK:- TableView Delegate
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let alertController = UIAlertController.init(title: "Selected Row", message: "You selected row \(indexPath.row)", preferredStyle: .Alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+        let alertController = UIAlertController.init(title: "Selected Row", message: "You selected row \(indexPath.row)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
             //print("Cancel Action")
         }
-        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
             //print("Ok Action")
         }
         alertController.addAction(OKAction)
         alertController.addAction(cancelAction)
         
-        self.presentViewController(alertController, animated: true) {
+        self.present(alertController, animated: true) {
             //print("Presented")
         }
     }
